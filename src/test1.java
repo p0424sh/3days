9c5c81a78b6fdfa4f8921ecf419348d39f0b7821
Hwani <p0424sh@naver.com>
2019-06-27 12:41:17 +0900

    test

---
+++ src/test1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        for (int i= 0;i<3;i++) {
+            String a = sc.nextLine();
+            System.out.println(a);
+        }
+
+    }
+}
---
+++ src/test4.java
@@ -0,0 +1,13 @@
+public class test4 {
+    public static void main(String[] args) {
+
+
+        for(int i=2; i<10; i++){
+            for (int j=1; j<=9; j++){
+                if(i%3==0)
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+
+        }
+    }
+}
